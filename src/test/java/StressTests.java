1ea459e7f0ee34a84b714d32c9448ebc5aef7348
tzhelyazkov <teodor.zhelyazkov99@gmail.com>
2023-03-01 14:22:54 +0200

    Added StressTests

---
+++ src/test/java/StressTests.java
@@ -0,0 +1,20 @@
+import org.example.Helper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.mockito.Mock;
+
+public class StressTests {
+    @Mock
+    Helper helper;
+
+    @BeforeEach
+    public void initialize() {
+        helper = new Helper();
+    }
+
+    @RepeatedTest(10000)
+    public void stressTest_shouldAlways_EndProgram_gracefully() {
+        helper.eventCreation();
+        helper.threadCreation();
+    }
+}
